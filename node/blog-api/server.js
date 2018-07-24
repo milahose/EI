c128b79fa7abf2437c6814a4dfdd4dc20056ede6
Mila Hose <mila.hose@gmail.com>
2018-07-24 11:23:40 -0700

    Update server

--- node/blog-api/server.js
+++ node/blog-api/server.js
@@ -7,6 +7,37 @@ const BlogPostRouter = require('./blogPostRouter.js');
 app.use(morgan('common'));
 app.use('/blog-posts', BlogPostRouter);
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Listening on port ${process.env.PORT || 3001}`);
-});
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 3001;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
