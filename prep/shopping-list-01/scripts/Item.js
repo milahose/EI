273ff2d3247e63b7a835dd3c6f2405e85ead4403
Mila Hose <mila.hose@gmail.com>
2018-08-20 00:42:35 -0700

    Add Item module

--- prep/shopping-list-01/scripts/Item.js
+++ prep/shopping-list-01/scripts/Item.js
@@ -1,3 +1,11 @@
 const Item = (function() {
-  return {};
+  const validateName = (name) => {
+    if (!name) throw new Error('Name does not exist.');
+  };
+
+  const create = (name) => {
+    return { id: cuid(), name, checked: false };
+  };
+
+  return { validateName, create };
 })();
