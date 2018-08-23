f7a840fcea636d639a85745ce65d63bc6a4f1917
Mila Hose <mila.hose@gmail.com>
2018-08-22 20:09:41 -0700

    Update store

--- prep/shopping-list-01/scripts/store.js
+++ prep/shopping-list-01/scripts/store.js
@@ -8,5 +8,59 @@ const store = (function() {
   let hideCheckedItems = false;
   let searchTerm = '';
 
-  return {items, hideCheckedItems, searchTerm};
+  const findById = (id) => {
+    return store.items.find(item => item.id === id);
+  };
+
+  const addItem = (name) => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+      render();
+    } catch (error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
+  };
+
+  const findAndToggleChecked = (id) => {
+    let foundItem = findById(id);
+    console.log('toggle checked found item', foundItem)
+    foundItem.checked = !foundItem.checked;
+  };
+
+  const findAndUpdateName = (id, newName) => {
+    try {
+      Item.validateName(newName);
+      let item = findById(id);
+      item.name = newName;
+    } catch (error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  };
+
+  const findAndDelete = (id) => {
+    const index = store.items.findIndex(item => item.id === id);
+    store.items.splice(index, 1);
+  };
+
+  const toggleCheckedFilter = () => {
+    store.hideCheckedItems = !store.hideCheckedItems;
+  };
+
+  const setSearchTerm = (val) => {
+    store.searchTerm = val;
+  };
+
+  return { 
+    items,
+    hideCheckedItems,
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
+    toggleCheckedFilter,
+    setSearchTerm
+  };
 })();
